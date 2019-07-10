726abe6f652375fe2fefefdadb1b1b82b7c892c8
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-07-10 11:57:40 -0300

    Criar front end da home nuvem de tags

--- wp-content/themes/sme-portal-institucional/classes/ModelosDePaginas/PaginaInicial/PaginaInicial.php
+++ wp-content/themes/sme-portal-institucional/classes/ModelosDePaginas/PaginaInicial/PaginaInicial.php
@@ -288,29 +288,33 @@ class PaginaInicial extends Util
 	    <?php
     }
 
-    public function montaHtmlNuvemTags(){
-	    echo '<div class="col">';
-	    $args = array(
-            'smallest'                  => 8,
-            'largest'                   => 22,
-            'unit'                      => 'pt',
-            'number'                    => 20,
-            'format'                    => 'flat',
-            'separator'                 => "\n",
-            'orderby'                   => 'name',
-            'order'                     => 'ASC',
-            'exclude'                   => null,
-            'include'                   => null,
-            'topic_count_text_callback' => default_topic_count_text,
-            'link'                      => 'view',
-            'taxonomy'                  => 'post_tag',
-            'echo'                      => true,
-            'show_count'                  => 0,
-            'child_of'                  => null, // see Note!
-            );
-	    wp_tag_cloud($args);
-	    echo '</div>';
-    }
+	public function montaHtmlNuvemTags(){
+
+		echo '<div class="col">';
+
+		$args = array(
+			'smallest'                  => 8,
+			'largest'                   => 22,
+			'unit'                      => 'pt',
+			'number'                    => 20,
+			'format'                    => 'flat',
+			'separator'                 => "\n",
+			'orderby'                   => 'name',
+			'order'                     => 'ASC',
+			'exclude'                   => null,
+			'include'                   => null,
+			'topic_count_text_callback' => default_topic_count_text,
+			'link'                      => 'view',
+			'taxonomy'                  => 'post_tag',
+			'echo'                      => true,
+			'show_count'                  => 0,
+			'child_of'                  => null, // see Note!
+		);
+
+		wp_tag_cloud($args);
+		
+		echo '</div>';
+	}
 
 	public function abreContainerHtmlIconesMenuIcones()
 	{
