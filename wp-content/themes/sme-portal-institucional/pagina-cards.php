e7a5971bb4d19b53aeff00668bc1f72e25664c1f
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-07-10 11:35:16 -0300

    Desenvolver Back End Página Participação Social

--- wp-content/themes/sme-portal-institucional/pagina-cards.php
+++ wp-content/themes/sme-portal-institucional/pagina-cards.php
@@ -1,13 +1,17 @@
 <?php
 /*
  * Template Name: Página Cards
- * Description: Página Cards, página que exibe título, texto, thumbnail, e os cards da taxonomia escolhida
+ * Description: Página Cards, página que exibe título, texto, thumbnail, e os cards da taxonomia escolhida. Inicialmente serve para Participação Social e Acesso a informação
  */
 
 use Classes\ModelosDePaginas\PaginaCards\PaginaCards;
 
 get_header();
+
+
 $pagina_cards = new PaginaCards();
+
+
 get_footer();
 
 
