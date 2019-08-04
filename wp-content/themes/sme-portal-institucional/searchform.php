c27306808a9fff0c9c1935428dd1b4cd5672f6aa
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-04 09:56:58 -0300

    Busca - Refatoração - eliminação template page

--- wp-content/themes/sme-portal-institucional/searchform.php
+++ wp-content/themes/sme-portal-institucional/searchform.php
@@ -1,7 +1,4 @@
 <?php
-/*
-Template Name: Search Form
-*/
 ?>
 <form id="demo-2" action="<?php echo home_url( '/' ); ?>" method="get" class="form-inline " style="padding-top: 12px;">
     <fieldset>
