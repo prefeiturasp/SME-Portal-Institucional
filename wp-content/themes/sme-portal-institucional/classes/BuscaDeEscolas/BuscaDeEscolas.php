8743d80325dfa5f2a70cc052d025ee5d51b7c2d1
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-12 11:14:24 -0300

    Tarefa 3790 - Acessibilidade - Busca Escolas

--- wp-content/themes/sme-portal-institucional/classes/BuscaDeEscolas/BuscaDeEscolas.php
+++ wp-content/themes/sme-portal-institucional/classes/BuscaDeEscolas/BuscaDeEscolas.php
@@ -16,26 +16,29 @@ class BuscaDeEscolas
         <section class="container">
 
             <form id="formulario_busca_escola">
-                <section class="form-row">
-                    <article class="form-group col-md-4">
-                        <label for="busca_escola">Busca de Escolas</label>
-                        <input type="text" class="form-control" id="busca_escola" name="busca_escola" placeholder="Digite o nome de uma escola">
-                    </article>
-                    <article class="form-group col-md-4">
-                        <label for="busca_tipo_de_escola">Tipo de Escola</label>
-                        <select class="form-control" id="busca_tipo_de_escola" name="busca_tipo_de_escola">
-                            <option value="">Selecione uam opção</option>
-                        </select>
-                    </article>
-                    <article class="form-group col-md-4">
-                        <label for="busca_dre">Busca DRE</label>
-                        <select class="form-control" id="busca_dre" name="busca_dre">
+                <fieldset>
+                    <legend>Busca de Escolas</legend>
+                    <section class="form-row">
+                        <article class="form-group col-md-4">
+                            <label for="busca_escola">Busca de Escolas</label>
+                            <input type="text" class="form-control" id="busca_escola" name="busca_escola" placeholder="Digite o nome de uma escola">
+                        </article>
+                        <article class="form-group col-md-4">
+                            <label for="busca_tipo_de_escola">Tipo de Escola</label>
+                            <select class="form-control" id="busca_tipo_de_escola" name="busca_tipo_de_escola">
+                                <option value="">Selecione uam opção</option>
+                            </select>
+                        </article>
+                        <article class="form-group col-md-4">
+                            <label for="busca_dre">Busca DRE</label>
+                            <select class="form-control" id="busca_dre" name="busca_dre">
 
 
-                        </select>
-                    </article>
-                    <button id="form_submit" name="form_submit" class="btn btn-primary" type="submit">Buscar Escolas</button>
-                </section>
+                            </select>
+                        </article>
+                        <button id="form_submit" name="form_submit" class="btn btn-primary" type="submit">Buscar Escolas</button>
+                    </section>
+                </fieldset>
             </form>
             <br>
 
