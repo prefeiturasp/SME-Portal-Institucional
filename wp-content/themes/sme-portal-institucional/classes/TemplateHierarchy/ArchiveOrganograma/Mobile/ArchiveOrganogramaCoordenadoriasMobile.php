10d62d951f75cd619a3fef8b79fec90731cf1595
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-23 15:58:04 -0300

    Corrigir o organograma - exibir todas as coordenadorias - 4260/4315 MOBILE

--- wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/ArchiveOrganograma/Mobile/ArchiveOrganogramaCoordenadoriasMobile.php
+++ wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/ArchiveOrganograma/Mobile/ArchiveOrganogramaCoordenadoriasMobile.php
@@ -17,7 +17,12 @@ class ArchiveOrganogramaCoordenadoriasMobile extends ArchiveOrganogramaMobile
 
 	public function getDadosCoordenadorias(){
 
-		$termos = get_terms(array('taxonomy' => self::TAXONOMIA, 'post_type' => self::CPT, 'parent' => self::$id_taxonomia_coordenadorias )); // ID Taxonomia Coordenadorias
+		$termos = get_terms(array(
+            'taxonomy' => self::TAXONOMIA,
+            'post_type' => self::CPT,
+            'parent' => self::$id_taxonomia_coordenadorias,
+			'hide_empty' => false,
+        )); // ID Taxonomia Coordenadorias
 
 		foreach ($termos as $termo){
 
