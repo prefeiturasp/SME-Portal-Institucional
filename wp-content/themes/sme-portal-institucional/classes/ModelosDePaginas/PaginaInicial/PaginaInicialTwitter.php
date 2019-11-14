1781965786d0f2bf191039112bd7447aa2d71102
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-11-14 09:55:15 -0300

    Card 8289 - Página Home - Implementar melhorias de acessibilidade para o selo

--- wp-content/themes/sme-portal-institucional/classes/ModelosDePaginas/PaginaInicial/PaginaInicialTwitter.php
+++ wp-content/themes/sme-portal-institucional/classes/ModelosDePaginas/PaginaInicial/PaginaInicialTwitter.php
@@ -15,7 +15,9 @@ class PaginaInicialTwitter extends PaginaInicial
 	public function montaHtmlTwitter(){
 		?>
 		<article class="container-twitter">
-			<a class="twitter-timeline" data-lang="pt" data-height="200" data-chrome = "noheader nofooter"  href="https://twitter.com/EducaPrefSP">Tweets by EducaPrefSP</a>
+            <a href="#irrodape" class=""><span class="esconde-item-acessibilidade">Pular feed de noticias do twiter</span> </a>
+
+            <a class="twitter-timeline" data-lang="pt" data-height="200" data-chrome = "noheader nofooter"  href="https://twitter.com/EducaPrefSP">Tweets by EducaPrefSP</a>
 			<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 		</article>
 		<?php
