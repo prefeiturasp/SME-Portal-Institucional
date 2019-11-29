3117d4c3cac9ca1f1b71e485e904c170b6e73783
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-11-29 09:42:33 -0300

    Card 8278 Listar no resultado da busca páginas que tenham outra páginas ascendentes

--- wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/Search/LoopSearch.php
+++ wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/Search/LoopSearch.php
@@ -8,6 +8,7 @@ class LoopSearch
 	protected $search;
 	protected $argsSearch;
 	protected $querySearch;
+	protected $verificaConteudoSearch = false;
 
 	public function __construct()
 	{
@@ -45,104 +46,82 @@ class LoopSearch
 	}
 
 
-	public function verificaConteudoSearch(){
+	public function verificaConteudoSearch()
+	{
 
 		$campo_de_busca = $_GET['s'];
 
+		$texto_buscado_organograma = array(
+			'CEDOC',
+			'cedoc',
+			'Centro de Documentação',
+			'Centro de Documentacao',
+			'centro de documentacao',
+			'CENTRO DE DOCUMENTAÇÃO',
+			'CENTRO DE DOCUMENTACAO',
+			'DOCUMENTACAO',
+			'documentacao',
+			'documentação',
+			'DOCUMENTAÇÃO',
+			'Documentação',
+			'Documentacao',
+			' Centro de Documentação (CEDOC)',
+			'Centro de Documentação – CEDOC',
+			'Centro de Documentação – cedoc',
+			'Centro de Documentação cedoc',
+			'Centro de Documentação - CEDOC');
+		if (in_array($campo_de_busca, $texto_buscado_organograma)) {
+			$this->montaHtmlSearchPersonalizado('Centro de Documentação – CEDOC', 'centro-de-multimeios/memoria-documental/centro-de-documentacao-cedoc/');
+		}
+
 		$texto_buscado_organograma = array('organograma', 'Organograma');
-		if (in_array($campo_de_busca,$texto_buscado_organograma)){
-		    $this->montaHtmlSearchPersonalizado('Organograma — Secretaria Municipal de Educação', 'organograma');
+		if (in_array($campo_de_busca, $texto_buscado_organograma)) {
+			$this->montaHtmlSearchPersonalizado('Organograma — Secretaria Municipal de Educação', 'organograma');
 		}
 
-		$texto_buscado_mapa_dres = array('Mapa Dres',  'Mapa das Dres', 'mapa dres', 'mapa das dres' , 'DRE s', 'dres');
-		if (in_array($campo_de_busca,$texto_buscado_mapa_dres)){
-            $this->montaHtmlSearchPersonalizado("DRE's — Diretorias Regionais de Educação", 'mapa-dres');
+		$texto_buscado_mapa_dres = array('Mapa Dres', 'Mapa das Dres', 'mapa dres', 'mapa das dres', 'DRE s', 'dres');
+		if (in_array($campo_de_busca, $texto_buscado_mapa_dres)) {
+			$this->montaHtmlSearchPersonalizado("DRE's — Diretorias Regionais de Educação", 'mapa-dres');
 		}
 
-		$texto_buscado_curriculo_da_cidade = array('Curriculo da Cidade',  'curriculo da cidade', 'Currículo da Cidade', 'currículo da cidade');
-		if (in_array($campo_de_busca, $texto_buscado_curriculo_da_cidade)){
+		$texto_buscado_curriculo_da_cidade = array('Curriculo da Cidade', 'curriculo da cidade', 'Currículo da Cidade', 'currículo da cidade');
+		if (in_array($campo_de_busca, $texto_buscado_curriculo_da_cidade)) {
 			$this->montaHtmlSearchPersonalizado("Currículo da Cidade", 'curriculo-da-cidade');
 		}
 
-		$texto_buscado_agenda = array('Agenda',  'agenda', 'Agenda do Secretário', 'agenda do secretário', 'Agenda do Secretario', 'agenda do secretario');
-		if (in_array($campo_de_busca, $texto_buscado_agenda)){
+		$texto_buscado_agenda = array('Agenda', 'agenda', 'Agenda do Secretário', 'agenda do secretário', 'Agenda do Secretario', 'agenda do secretario');
+		if (in_array($campo_de_busca, $texto_buscado_agenda)) {
 			$this->montaHtmlSearchPersonalizado("Agenda do Secretário de Educação", 'agenda');
 		}
 
-		$texto_buscado_busca_de_escolas = array('Escolas',  'escola', 'Busca de Escolas', 'busca de escolas', 'busca de escola', 'encontrar uma escola');
-		if (in_array($campo_de_busca, $texto_buscado_busca_de_escolas)){
+		$texto_buscado_busca_de_escolas = array('Escolas', 'escola', 'Busca de Escolas', 'busca de escolas', 'busca de escola', 'encontrar uma escola');
+		if (in_array($campo_de_busca, $texto_buscado_busca_de_escolas)) {
 			$this->montaHtmlSearchPersonalizado("Encontre a Escola Desejada", 'busca-de-escolas');
 		}
 
-    }
+		$this->verificaConteudoSearch = true;
 
-	public function montaHtmlSearchPersonalizado($titulo, $url){
+	}
 
-			?>
-            <div class="container">
-                <div class="row mb-4">
-                    <div class="col-lg-12 pb-4 border-bottom">
-                        <h4 class="fonte-dezoito font-weight-bold mb-2">
-                            <a class="text-decoration-none text-dark" href="<?= STM_URL.'/'.$url ?>">
-                               <?= $titulo ?>
-                            </a>
-                        </h4>
-                        <div class="col-12">
-                            <a class="btn btn-primary" href="<?= STM_URL.'/'.$url ?>"><?php echo VEJAMAIS ?></a>
-                        </div>
+	public function montaHtmlSearchPersonalizado($titulo, $url)
+	{
+
+		?>
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col-lg-12 pb-4 border-bottom">
+                    <h4 class="fonte-dezoito font-weight-bold mb-2">
+                        <a class="text-decoration-none text-dark" href="<?= STM_URL . '/' . $url ?>">
+							<?= $titulo ?>
+                        </a>
+                    </h4>
+                    <div class="col-12">
+                        <a class="btn btn-primary" href="<?= STM_URL . '/' . $url ?>"><?php echo VEJAMAIS ?></a>
                     </div>
                 </div>
             </div>
-            <?php
-    }
-
-	public function montaQueryPaginasFilhas($id_page)
-	{
-
-		$childArgs = array(
-			'sort_order' => 'ASC',
-			'sort_column' => 'menu_order',
-			'child_of' => $id_page
-		);
-
-		$childList = get_pages($childArgs);
-
-		if ($childList) {
-			?>
-
-			<?php
-			foreach ($childList as $child) {
-				$img_destacada = get_the_post_thumbnail_url($child->ID);
-				$alt = get_post_meta(get_post_thumbnail_id($child->ID), '_wp_attachment_image_alt', true);
-				?>
-                <div class="row mb-4">
-                    <div class="col-lg-12 pb-4 border-bottom">
-						<?php
-
-						if ($img_destacada) {
-							echo '<figure class=" m-0">';
-							echo '<img class="img-fluid rounded float-left mr-4 w-25" src="' . $img_destacada . '" alt="' . $alt . '">';
-							echo '</figure>';
-						}
-						?>
-                        <h4 class="fonte-dezoito font-weight-bold mb-2">
-                            <a class="text-decoration-none text-dark" href="<?php the_permalink() ?>">
-								<?= $child->post_title ?>
-                            </a>
-                        </h4>
-                        <p class="fonte-dezesseis mb-2">
-							<?= get_the_excerpt($child->ID) ?>
-                        </p>
-
-                        <div class="col-12">
-                            <a class="btn btn-primary" href="<?= $child->guid ?>"><?php echo VEJAMAIS ?></a>
-                        </div>
-
-                    </div>
-                </div>
-
-			<?php }
-		}
+        </div>
+		<?php
 	}
 
 	public function montaHtmlSearch()
@@ -151,13 +130,9 @@ class LoopSearch
 
         <div class="container">
 
-			<?php if ($this->querySearch->have_posts()) :
+			<?php if ($this->querySearch->have_posts() || $this->verificaConteudoSearch) :
 
 				while ($this->querySearch->have_posts()) : $this->querySearch->the_post();
-
-					if (get_post_type(get_the_ID()) === 'page') {
-						$this->montaQueryPaginasFilhas(get_the_ID());
-					}
 					?>
                     <div class="row mb-4">
                         <div class="col-lg-12 pb-4 border-bottom">
@@ -185,7 +160,12 @@ class LoopSearch
                         </div>
                     </div>
 
-				<?php
+					<?php
+
+					if (get_post_type(get_the_ID()) === 'page') {
+						$this->montaQueryPaginasFilhas(get_the_ID());
+					}
+
 				endwhile;
 			else:
 				$this->montaHtmlNenhumPostEncontrado();
@@ -206,6 +186,55 @@ class LoopSearch
         </div>
 
 		<?php
+	}
+
+	public function montaQueryPaginasFilhas($id_page)
+	{
+
+		$childArgs = array(
+			'sort_order' => 'ASC',
+			'sort_column' => 'menu_order',
+			'child_of' => $id_page,
+			'posts_per_page' => -1
+		);
+
+		$childList = get_pages($childArgs);
+
+		if ($childList) {
+			?>
+
+			<?php
+			foreach ($childList as $child) {
+				$img_destacada = get_the_post_thumbnail_url($child->ID);
+				$alt = get_post_meta(get_post_thumbnail_id($child->ID), '_wp_attachment_image_alt', true);
+				?>
+                <div class="row mb-4">
+                    <div class="col-lg-12 pb-4 border-bottom">
+						<?php
+						if ($img_destacada) {
+							echo '<figure class=" m-0">';
+							echo '<img class="img-fluid rounded float-left mr-4 w-25" src="' . $img_destacada . '" alt="' . $alt . '">';
+							echo '</figure>';
+						}
+						?>
+                        <h4 class="fonte-dezoito font-weight-bold mb-2">
+                            <a class="text-decoration-none text-dark" href="<?php the_permalink() ?>">
+								<?= $child->post_title ?>
+                            </a>
+                        </h4>
+                        <p class="fonte-dezesseis mb-2">
+							<?= get_the_excerpt($child->ID) ?>
+                        </p>
+
+                        <div class="col-12">
+                            <a class="btn btn-primary" href="<?= $child->guid ?>"><?php echo VEJAMAIS ?></a>
+                        </div>
+
+                    </div>
+                </div>
+
+			<?php }
+		}
 	}
 
 	public function montaHtmlNenhumPostEncontrado()
