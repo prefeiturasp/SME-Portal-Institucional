1c79598e1bc81f693c10b11ba22f599c56f1d882
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-11 08:52:20 -0300

    Atualizando Repositorio

--- wp-content/themes/sme-portal-institucional/functions.php
+++ wp-content/themes/sme-portal-institucional/functions.php
@@ -503,49 +503,7 @@ function redireciona_paginas_pendentes(){
 
 	}
 }
-/*add_action('template_redirect', 'redireciona_paginas_pendentes');
-
-function doctype_opengraph($output) {
-	return $output . '
-    xmlns:og="http://opengraphprotocol.org/schema/"
-    xmlns:fb="http://www.facebook.com/2008/fbml"';
-}
-add_filter('language_attributes', 'doctype_opengraph');
-
-function fb_opengraph() {
-	global $post;
-
-	if(is_single()) {
-		if(has_post_thumbnail($post->ID)) {
-			$img_src = wp_get_attachment_image_src(get_post_thumbnail_id( $post->ID ), 'full');
-		} else {
-			$img_src=STM_URL."/wp-content/uploads/2019/07/EDUCAÇÃO-1.png";
-			//$img_src = get_stylesheet_directory_uri() . '/img/opengraph_image.jpg';
-		}
-		if($excerpt = $post->post_excerpt) {
-			$excerpt = strip_tags($post->post_excerpt);
-			$excerpt = str_replace("", "'", $excerpt);
-		} else {
-			$excerpt = get_bloginfo('description');
-		}
-		*/?><!--
-
-		<meta property="og:title" content="<?php /*the_title(); */?>"/>
-		<meta property="og:description" content="<?php /*echo $excerpt; */?>"/>
-		<meta property="og:type" content="article"/>
-		<meta property="og:url" content="<?php /*the_permalink(); */?>"/>
-		<meta property="og:site_name" content="<?php /*echo get_bloginfo(); */?>"/>
-		<meta property="og:image" content="<?php /*echo $img_src[0]; */?>"/>
-		<meta property="fb:app_id" content="2538622436409878"/>
-
-		--><?php
-/*	} else {
-		return;
-	}
-}
-add_action('wp_head', 'fb_opengraph', 5);
-*/
-
+add_action('template_redirect', 'redireciona_paginas_pendentes');
 
 /*//Add Open Graph Meta Info from the actual article data, or customize as necessary
 function facebook_open_graph() {
@@ -564,7 +522,6 @@ function facebook_open_graph() {
 
 	//You'll need to find you Facebook profile Id and add it as the admin
 	//echo '<meta property="fb:admins" content="XXXXXXXXX-fb-admin-id"/>';
-	echo '<meta property="fb:app_id" content="2538622436409878"/>';
 	echo '<meta property="og:title" content="' . get_the_title() . '"/>';
 	echo '<meta property="og:description" content="' . $excerpt . '"/>';
 	echo '<meta property="og:type" content="article"/>';
