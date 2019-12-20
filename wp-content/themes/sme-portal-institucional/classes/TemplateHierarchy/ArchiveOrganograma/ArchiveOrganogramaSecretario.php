d47ef6771a710684c8a0e3e14cda8cb718b0368a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-20 13:02:00 -0300

    Card 8816 - Exibir nome do novo Secretário e Adjunto no organograma

--- wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/ArchiveOrganograma/ArchiveOrganogramaSecretario.php
+++ wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/ArchiveOrganograma/ArchiveOrganogramaSecretario.php
@@ -14,6 +14,8 @@ class ArchiveOrganogramaSecretario extends ArchiveOrganograma
 	{
 		// Secretario
 		$this->setTaxonomyName(self::SLUG_TAXONOMIA_SECRETARIO);
+		// Adjunto
+		$this->setTaxonomyAdj(self::SLUG_TAXONOMIA_ADJUNTO);
 		$classes_divs_secretario = array('card-deck', 'card shadow-sm border-0 bg-azul-escuro text-white text-center font-weight-bold mt-4 mb-2', 'card-body');
 		$classe_p_secretario = 'card-text';
 		$this->abreDivs(null, $classes_divs_secretario);
