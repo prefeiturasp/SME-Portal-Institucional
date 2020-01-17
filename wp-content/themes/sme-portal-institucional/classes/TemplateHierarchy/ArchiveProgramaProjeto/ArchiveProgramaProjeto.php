77263c3e243f79942a8c94332b6accc2c7f64ed8
rafaelhsouza-dev <58427157+rafaelhsouza-dev@users.noreply.github.com>
2020-01-17 18:48:47 -0300

    Card 10368 - Criando CPT ProgramaProjeto

---
+++ wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/ArchiveProgramaProjeto/ArchiveProgramaProjeto.php
@@ -0,0 +1,10 @@
+<?php
+namespace Classes\TemplateHierarchy\ArchiveProgramaProjeto;
+
+class ArchiveProgramaProjeto{
+	public function __construct(){
+		echo "<h1>Entrei na Classe</h1>";
+	}
+}
+
+?>
--- wp-content/themes/sme-portal-institucional/single-card.php
+++ wp-content/themes/sme-portal-institucional/single-card.php
@@ -3,3 +3,4 @@ use Classes\TemplateHierarchy\LoopSingleCard;
 get_header();
 $loop_single_card = new LoopSingleCard();
 get_footer();
+ 
