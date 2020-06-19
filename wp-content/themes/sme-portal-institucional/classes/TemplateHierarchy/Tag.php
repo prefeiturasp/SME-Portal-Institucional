11adb397fb62c832c355bd5d000f47df5aa58034
rafaelhsouza-dev <58427157+rafaelhsouza-dev@users.noreply.github.com>
2020-06-19 15:02:53 -0300

    criado paginação e placeholder de imagem para tags

--- wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/Tag.php
+++ wp-content/themes/sme-portal-institucional/classes/TemplateHierarchy/Tag.php
@@ -1,72 +1,73 @@
 <?php
 
 namespace Classes\TemplateHierarchy;
-use Classes\Lib\Util;
 
-class PaginaTag
-{
-	public function __construct()
-	{
-		$this->page_id = $page_id;
-		//$this->montaHtmlPaginaTag();
-		$this->montaHtmlTAG();
-
+class PaginaTag{
+	
+	public function __construct(){
+		$this->montaHTMLTags();
 	}
 	
-	public function montaHtmlTAG(){
+	public function montaHTMLTags() {
+		$term_id        = get_query_var('tag_id');
+		$arg           = 'include=' . $term_id;
+		$terms          = get_terms( 'post_tag', $arg );
+		$get_term_name  = $terms[0]->name;
+		$paged = ( get_query_var( 'page' ) ) ?  get_query_var( 'page' ) : 1;
+		$args = array(
+			'post_type' => 'post',
+			'post_status' => 'published',
+			'taxonomy'	=> 'post_tag',
+			'tag' => $get_term_name,
+			'posts_per_page' => 12,
+			'paged' => $paged,
+		);
+		$the_query = new \WP_Query( $args );
 		?>
 		<div class="container mt-4 mb-4">
 			<section class="row container-post-categorias">
-			<?php
-			while (have_posts()) : the_post();
-
-				$post_thumbnail_id = get_post_thumbnail_id( $post_id );
-				$image_alt = get_post_meta( $post_thumbnail_id, '_wp_attachment_image_alt', true);
-				?>
+		<?php
+		while ( $the_query->have_posts() ) : $the_query->the_post();
+		?>
 				<article class='col-12 col-md-4 mt-4 mb-4'>
 					<?php if (has_post_thumbnail()) { ?>
 						<figure>
 							<img alt="<?= $image_alt ?>" class="img-fluid aligncenter img-thumbnail" src="<?= get_the_post_thumbnail_url() ?>"/>
+						</figure>
+					<?php }else{ ?>
+						<figure>
+							<img alt="SME Prefeitura" class="img-fluid aligncenter img-thumbnail" src="https://educacao.sme.prefeitura.sp.gov.br/wp-content/uploads/2020/06/placeholder-sme.jpg"/>
 						</figure>
 					<?php } ?>
 					<h3 class="titulo-post-categorias"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 					<?php the_excerpt(); ?>
 
 				</article>
-				<?php
-				$conta_posts++;
-			endwhile;
+		<?php
+		endwhile;
+		?>
+		</section>
+		</div>	
+		<?php	
+		wp_reset_postdata();
+		?>
+		<div class="paginacao-atual">
+			<?php
+			echo paginate_links( array(
+				'base' => @add_query_arg('page','%#%'),
+				'current' => $paged,
+				'total'   => $the_query->max_num_pages,
+				'end_size'  => 1,
+				'mid_size'  => 2,
+				'show_all' => false,
+				'prev_next' => true,
+				'prev_text' => __('<<'),
+				'next_text' => __('>>'),
+			) );
 			?>
-			</section>
 		</div>
 		<?php
 	}
 	
-	public function montaHtmlPaginaTag(){
-
-		echo '<section class="container">';
-		if (have_posts()) : while (have_posts()) : the_post();
-			?>
-			<article class="row">
-				<article class="col-lg-12 col-xs-12">
-					<h1 class="mb-4" id="<?= $this->page_slug ?>"><?php the_title(); ?></h1>
-				</article>
-			</article>
-
-
-			<article class="row">
-				<article class="col-lg-9 col-xs-12">
-
-					<?php the_content(); ?>
-				</article>
-			</article>
-		<?php
-		endwhile;
-		endif;
-		wp_reset_query();
-		echo '</section>'; //container
-	}
-	
 }
-
 ?>
