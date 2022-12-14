85c4560b71d982fe174a28c67dea9e0c6da0b332
Felipe Viana <flpnasa@gmail.com>
2022-12-14 15:58:36 -0300

    Incluir eventos por periodo

--- wp-content/themes/intranet/classes/TemplateHierarchy/ArchiveAgendaNew/ArchiveAgendaAjaxCalendarioNew.php
+++ wp-content/themes/intranet/classes/TemplateHierarchy/ArchiveAgendaNew/ArchiveAgendaAjaxCalendarioNew.php
@@ -30,10 +30,31 @@ class ArchiveAgendaAjaxCalendarioNew extends Util
 		
 		$args = array(
 			'post_type' => 'agendanew',
-			'meta_key'     => 'data_do_evento',
-			'meta_value'   => $data_recebida_ao_clicar, // change to how "event date" is stored
-			'meta_compare' => '=',
-			'posts_per_page' => -1
+			'posts_per_page' => -1,
+			'meta_query' => array(
+				'relation' => 'OR',
+				array(
+					'key' => 'data_do_evento',
+					'value' => $data_recebida_ao_clicar,
+					'compare' => '=',
+				),
+				array(
+					'relation' => 'AND',
+					array(
+						'key' => 'data_do_evento',
+						'value' => $data_recebida_ao_clicar,
+						'type' => 'date',
+						'compare' => '<='
+					),
+					array(
+						'key' => 'data_evento_final',
+						'value' => $data_recebida_ao_clicar,
+						'type' => 'date',
+						'compare' => '>='
+					),
+				),			
+			)
+			
 		);
 		$query = new \WP_Query( $args );
 
@@ -48,7 +69,7 @@ class ArchiveAgendaAjaxCalendarioNew extends Util
 						//echo "</pre>";
 					?>
 					<?php foreach($eventos as $evento): ?>
-						<div class="agenda mb-4 agenda-new aaa">
+						<div class="agenda mb-4 agenda-new abc">
 							<div class="order_hri">
 								<?php
 									//converte campo hora por extenso para ordenar
@@ -141,5 +162,3 @@ class ArchiveAgendaAjaxCalendarioNew extends Util
 	}
 
 }
-
-?>
--- wp-content/themes/intranet/construtor/construtor-calendario_1_1.php
+++ wp-content/themes/intranet/construtor/construtor-calendario_1_1.php
@@ -1,7 +1,65 @@
 <?php
 use Classes\ModelosDePaginas\PaginaAgendaSecretario\PaginaAgendaSecretario;
 use Classes\TemplateHierarchy\ArchiveAgendaNew\ArchiveAgendaNew;
+
+function data_periodo($dataIni, $dataFin){
+	$dataIni = implode('-', array_reverse(explode('/', $dataIni)));
+	$dataFin = implode('-', array_reverse(explode('/', $dataFin)));
+
+    $inicial = new DateTime( $dataIni );
+    $final = new DateTime( $dataFin );
+    $final = $final->modify( '+1 day' ); 
+
+    $intervalo = new DateInterval('P1D');
+    $periodo = new DatePeriod($inicial, $intervalo ,$final);
+
+    return $periodo;
+}
+
+$datas = array();
+
+$args = array(
+    'post_type' => 'agendanew',    
+    'posts_per_page' => -1
+);
+$query = new \WP_Query( $args );
+
+if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+    $tipo = get_field('tipo_de_data');
+    if($tipo){
+        $dataIni = get_field('data_do_evento');
+	    $dataFin = get_field('data_evento_final');
+        $periodo = data_periodo($dataIni, $dataFin);
+        foreach ($periodo as $key => $value) {
+            $datas[] = $value->format('d/m/Y');      
+        }
+    } else {
+        $datas[] = get_field('data_do_evento');
+    }
+    
+endwhile;
+
+endif;
+wp_reset_postdata();
+
+$marc = '[';
+$i = 0;
+foreach($datas as $data){
+    $data = str_replace('/', '\/', $data);
+    if($i == 0){
+        $marc .= '&quot;' . $data . '&quot;';
+    } else {
+        $marc .= ',&quot;' . $data . '&quot;';
+    }
+    $i++;
+}
+
+$marc .= ']';
+
 ?>
+
+<input type="hidden" name="array_datas_agenda" id="array_datas_agenda" value="<?= $marc; ?>">
+
 <style>
     .agenda{
         display: none;
@@ -11,6 +69,7 @@ use Classes\TemplateHierarchy\ArchiveAgendaNew\ArchiveAgendaNew;
         display: block;
     }
 </style>
+
 <div class="container">
     <div class="calendario-construtor">
         <h3>Calendário Escolar</h3>
